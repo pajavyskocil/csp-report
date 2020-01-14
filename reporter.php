cce643e86d48953824123f14839d5fabda734880
Pavel Břoušek <melanger@users.noreply.github.com>
2020-01-14 10:39:18 +0100

    JSON

--- reporter.php
+++ reporter.php
@@ -7,23 +7,7 @@ include('Database.php');
     <head>
         <h1>Content security policy reporter</h1>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <?php
-        $raw_data = Database::getAll();
-
-        $lines = [];
-        foreach ($raw_data as $row) {
-            $line = '[\''. $row['time'] . '\',\'' . $row['ip'] . '\',\'' . $row['referrer'] . '\',\'' . $row['user_agent'] . '\',\'' .
-                    $row['report_blocked_uri'] . '\',\'' . $row['report_disposition'] . '\',\'' .
-                    $row['report_document_uri'] . '\',\'' . $row['report_effective_directive'] . '\',\'' .
-                    str_ireplace("'", '"', $row['report_original_policy']) . '\',\'' .
-                    $row['report_referrer'] . '\',\'' .
-                    $row['report_script_sample'] . '\',\'' . $row['report_status_code'] . '\',\'' .
-                    $row['report_violated_directive'] . '\']';
-            $lines[] = $line;
-        }
-        $data .= '[' . implode(",", $lines). ']';
-
-        echo "        <script type=\"text/javascript\">
+        <script type="text/javascript">
             google.charts.load('current', {'packages':['table']});
             google.charts.setOnLoadCallback(drawTable);
 
@@ -42,14 +26,13 @@ include('Database.php');
                 data.addColumn('string', 'report_script_sample');
                 data.addColumn('string', 'report_status_code');
                 data.addColumn('string', 'report_violated_directive');
-                data.addRows(" . $data . ");
+                data.addRows(JSON.parse("<?php echo htmlspecialchars(json_encode(Database::getAll())); ?>"));
 
                 var table = new google.visualization.Table(document.getElementById('table_div'));
 
                 table.draw(data, {page: 'enabled',pageSize: 10, showRowNumber: true, width: '90%', height: '100%'});
             }
-        </script>";
-        ?>
+        </script>
         <!--<form action="#">-->
 <!--             From: <input type="date" name="from">-->
 <!--             TO: <input type="date" name="to">-->
